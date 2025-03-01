3820e34b15c01958117344accd04fc910ba414af
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-01 14:23:23 +0530

    Naam Simran Singh Add: Checking if number is divisible by 5

--- DivisibleBy5.java
+++ DivisibleBy5.java
@@ -4,11 +4,8 @@ class DivisibleBy5{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the number: ");
 		int number = sc.nextInt();
-		if(number%5==0){
-			System.out.print("Is the number "+number+" divisible by 5? Yes");
-		}
-		else{
-			System.out.print("Is the number "+number+" divisible by 5? No");
-		}
+		
+		boolean isDivisible = (number%5==0);
+		System.out.print("Is the number "+number+" divisible by 5? "+isDivisible);
 	}
 }
