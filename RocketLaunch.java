85450fa0d28ec39cf99fe2242fb001a153814da0
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-01 21:17:30 +0530

    Naam Simran Singh Add: Countdown for rocket launch

---
+++ RocketLaunch.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class RocketLaunch{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the count down: ");
+		int counter = sc.nextInt();
+		while(counter!=0){
+			System.out.println(counter);
+			counter--;
+		}
+		System.out.println("Rocket Launched!");
+	}
+}
