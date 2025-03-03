918f17d143adbc84f3224e922dc41e0c379a594e
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-03 09:32:19 +0530

    Naam Simran Singh Add: Finding bonus of employees

---
+++ EmployeeBonus.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your salary: ");
+        double salary = sc.nextDouble();
+        System.out.print("Enter your years of service: ");
+        int years = sc.nextInt();
+
+        if (years > 5) {
+            double bonus = 0.05 * salary;
+            System.out.println("Your bonus is: INR " + bonus);
+        } else {
+            System.out.println("No bonus awarded.");
+        }
+
+        sc.close();
+    }
+}
