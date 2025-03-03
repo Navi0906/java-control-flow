567e6c8af6d0f18b49d1744f4718cd918331a7b4
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-03 22:04:02 +0530

    Naam Simran Singh Add: Printing number replacing multiple of 3, 5 with Fuzz, Buzz respectively

---
+++ FizzBuzz.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+		int i =1;
+
+		// checking if the entered number is positive number
+        if (number < 1) {
+            System.out.println("Invalid number.");
+        }
+		// printing numbers wiht condition where multiple of 3 print "Fizz", multiple of 5 print "Buzz" and both print "FizzBuzz" 
+		else {
+            while(i!=number+1) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+				i++;
+            }
+        }
+    }
+}
