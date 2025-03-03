27696e35c83ec50e665556c99e8934a4191a5236
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-03 23:06:01 +0530

    Naam Simran Singh Add: Gregorian calendar

---
+++ DayOfWeek.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+
+class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        
+        System.out.print("Enter month : ");
+        int month = sc.nextInt(); 
+        
+        System.out.print("Enter day : ");
+        int day = sc.nextInt(); 
+        
+        System.out.print("Enter year: ");
+        int year = sc.nextInt(); 
+
+		// Gregorian calender
+        int y0 = year - (14 - month) / 12; 
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400; 
+        int m0 = month + 12 * ((14 - month) / 12) - 2; 
+        int d0 = (day + x + (31 * m0) / 12) % 7; 
+
+        System.out.println("Day of the week: " + d0); // Output result
+
+        
+    }
+}
